28c74a7867ac8cb2f02de8471cf77933667a115b
AZMindroma <AZMindroma@github.com>
2024-12-12 14:03:21 +0100

    Erste Aufgabe

--- Programmieren/Code/main.c
+++ Programmieren/Code/main.c
@@ -2,7 +2,8 @@
 #include<stdio.h>
 
 int main() {
-    printf("Hello World!\n");
-    printf("Lol");
-    return 0;
+    printf("Hello World!\n\n");
+    printf("Please enter a character: ");
+    char scannedChar = getchar();
+    printf("You entered %c as character\n", scannedChar);
 }
