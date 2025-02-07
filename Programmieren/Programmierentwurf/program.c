8999412d830aa647606eef85db6c3f6c3f8a79b5
AZMindroma <AZMindroma@github.com>
2025-02-07 15:17:18 +0100

    Progress self-study programming

--- Programmieren/Programmierentwurf/program.c
+++ Programmieren/Programmierentwurf/program.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
 #include<cjson/cJSON.h>
+// External dependency. <ADD PACKAGE NAMES FOR APT>
+
+struct comic {
+    int comicID;
+    char comicSeries[35+1];
+    char comicName[35+1];
+    int pages;
+    struct comic * prev;
+    struct comic * next; 
+};
+
+struct comic * ptr = NULL;
+
+// Function prototypes
+int write_to_json(char name[], int age, char email[]);
 
 int main() {
     write_to_json("AZ", 18, "mail@azmindroma.de");
