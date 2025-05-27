56d362a4240ecd7c5a47b923b2b75241d5ac6c38
AZMindroma <69211110+AZMindroma@users.noreply.github.com>
2025-05-27 23:14:30 +0200

    Bis Aufgabe 1 in Block 3

---
+++ Semester2/Programmieren II/Code/src/Block3/Aufgabe2_Bruchzahlen/Bruchzahl.java
@@ -0,0 +1,16 @@
+package Block3.Aufgabe2_Bruchzahlen;
+
+public class Bruchzahl {
+    private long zaehler;
+    private long nenner;
+
+    public Bruchzahl(long z, long n) {
+        this.zaehler = z;
+        this.nenner = n;
+    }
+
+    // 1/2 + 7/9 = 1/2 * 9/9 + 7/9 * 2/2 = 9/18 + 14/18 = 23/18
+    public Bruchzahl addiere(Bruchzahl q) {
+
+    }
+}
--- Semester2/Programmieren II/Code/src/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-	public static void main(String[] args) {
-		int a = 5;
-		System.out.println(a++); // Zeige Wert (5), erhöhe dann auf 6
-		System.out.println(++a); // Erhöhe auf 7, zeige dann Wert (7)
-	}
-}
